3277cd294ca84b625686481f011bc312d67f201f
EsteNoEsMonti <estenoesmonti@gmail.com>
2020-03-02 16:48:51 -0300

    Commit: Creacion del Modelo de Mascota Perdida, Models/PetLost.js

---
+++ servidor/models/PetLost.js
@@ -0,0 +1,56 @@
+const mongoose = require('mongoose');
+
+const PetLostSchema = mongoose.Schema({
+    // Mascota
+    pet: {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: 'Pet'
+    },
+
+    // fecha de perdida
+    date: {
+        type: Date,
+        require: true
+    },
+
+    // Ubicacion
+    ubication: {
+        type: String,
+        require: true
+    },
+
+    // + Fotos
+    pics: {
+        type: String, //pasa la url
+        require: true
+    },
+
+    // Recompensa
+    reward: {
+        type: String,
+        require: false,
+        trim: true
+    },
+
+    //Dueño/s
+    owner: {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: 'User'
+    },
+    
+    //Telefono de contacto
+    telefon: {
+        type: String,
+        require: false
+    },
+
+
+    //Fecha de Creacion
+    created: {
+        type: Date,
+        default: Date.now()
+    }
+
+});
+
+module.exports = mongoose.model('PetLost', PetLostSchema);
