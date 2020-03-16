5dd0563cac5689be1fb8a10b25919d51efee13a0
unknown <mkluna@institutosanmartin.edu.ar>
2020-03-16 17:11:36 -0300

    Cambios en mi rama

--- cliente/src/App.js
+++ cliente/src/App.js
@@ -7,7 +7,8 @@ import AlertState from './context/alert/alertState';
 import AuthState from "./context/authentication/authState";
 import inicio from './components/inicio/inicio';
 import Header from "./components/layout/header";
-import InicioApp from './components/inicio/InicioApp'
+import InicioApp from './components/inicio/InicioApp';
+import Tarjeta from './components/tarjetas/tarjeta';
 
 
 
@@ -23,6 +24,7 @@ function App() {
               <Route exact path="/login" component={Login} />
               <Route exact path="/new-account" component={NewAccount} />
               <Route exact path="/inicio" component={InicioApp}/>
+              <Route exact path="/tarjeta" component={Tarjeta}/>
             </Switch>
         </Router>  
       </AuthState>
